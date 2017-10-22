258f44379bc565c7443298db9a1fc0326a6f6ae2
BenjaminBLi <li.benjamin.b@gmail.com>
2017-10-22 04:24:17 -0400

    Created basic function for generating melody

--- FuncToList.py
+++ FuncToList.py
@@ -16,7 +16,31 @@ scales =\
         },
     }
 
+chords =\
+[
+    [
+        ["A", "C”, “E”, “G"],
+        ["D", "F", "A", "C"],
+        ["A", "C", "E", "G"],
+        ["A", "C", "E", "G"],
+        ["D", "F", "A", "C"],
+        ["D", "F", "A", "C"],
+        ["A", "C", "E", "G"],
+        ["A", "C", "E", "G"],
+        ["E", "G", "B", "D"],
+        ["D", "F", "A", "C"],
+        ["A", "C", "E", "G"],
+        ["E", "G", "B", "D"],
+    ]
+]
+
 def genScale(type, ch):
+    """
+    generates the given type of scale: minor pentatonic or blues, given a starting note
+    :param type: string
+    :param ch: string
+    :return: list
+    """
     ret = []
     for i in range(1, 6):
         for let in scales[type][ch]:
@@ -24,6 +48,13 @@ def genScale(type, ch):
     return ret
 
 def getMelody(type, let, clst):
+    """
+    returns the melody given the an integer input array from ImgToFunc
+    :param type: string
+    :param let: string
+    :param clst: list
+    :return: list
+    """
     ret = []
     cScale = genScale(type, let)
     scaleLen = len(cScale)
@@ -33,4 +64,23 @@ def getMelody(type, let, clst):
 
     return ret
 
-print(getMelody("pentatonic", "A", imgToFunc("https://i.ytimg.com/vi/RvaNnvl2Q8g/maxresdefault.jpg", 2)[0]))
+def getParts(type, let, lists):
+    """
+    generates the notes to be played given the lists in lists
+    :param type: string
+    :param let: string
+    :param lists: list of lists
+    :return: list of lists
+    """
+    parts = []
+    for list in lists:
+        parts.append(getMelody(type, let, list))
+    return parts
+    background = [];
+    chordLen = len(chords)
+    for i in range(len(parts)):
+        background.append(chords[i%chordLen])
+    parts.append(background)
+    return parts
+
+#print(getMelody("pentatonic", "A", imgToFunc("https://i.ytimg.com/vi/RvaNnvl2Q8g/maxresdefault.jpg", 2)[0]))
