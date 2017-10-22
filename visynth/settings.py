0ed411363fb6f80bbdd1c2d41b42180bd8274a16
DoomPotato <shaar@utschools.ca>
2017-10-22 13:51:59 -0400

    0_o

--- visynth/settings.py
+++ visynth/settings.py
@@ -121,4 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = ''
-STATICFILES_DIRS = ("visynthapp/templates/static",)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+# WHAT THE ACTUAL FUCK...
